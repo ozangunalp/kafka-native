ccaed91ed35405868e7838306bf435bcd1c07df1
Ozan Gunalp <ozangunalp@gmail.com>
2023-07-13 17:20:25 +0200

    Switch to using jakarta namespace

--- kafka-server/src/main/java/com/ozangunalp/kafka/server/Startup.java
+++ kafka-server/src/main/java/com/ozangunalp/kafka/server/Startup.java
@@ -1,8 +1,8 @@
 package com.ozangunalp.kafka.server;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
 
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.common.utils.Utils;
