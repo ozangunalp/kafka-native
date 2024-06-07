ozangunalp/kafka-native

commit 3fd9fa9694b399dd7bd76b2c51fcd72777cb01e8

add unit tests for Storage#parseScram

kafka-server/src/main/java/com/ozangunalp/kafka/server/ServerConfig.java
@@ -31,6 +31,17 @@ public interface ServerConfig {
     @WithDefault("true")
     boolean autoConfigure();
 
+    /**
+     * List of scram credentials.
+     * <br/>
+     * Format of the scram string must be in one of the following forms:
+     * <plaintext>
+     * SCRAM-SHA-256=[user=alice,password=alice-secret]
+     * SCRAM-SHA-512=[user=alice,iterations=8192,salt="N3E=",saltedpassword="YCE="]
+     * </plaintext>
+
+     * @return list of scram credentials
+     */
     Optional<List<String>> scramCredentials();
 
 }

kafka-server/src/main/java/com/ozangunalp/kafka/server/Storage.java
@@ -76,23 +76,26 @@ public final class Storage {
         return new ApiMessageAndVersion(message, (short) 0);
     }
 
-    public static ApiMessageAndVersion metadataVersionMessage(MetadataVersion metadataVersion) {
+    private static ApiMessageAndVersion metadataVersionMessage(MetadataVersion metadataVersion) {
         return withVersion(new FeatureLevelRecord().
                 setName(MetadataVersion.FEATURE_NAME).
                 setFeatureLevel(metadataVersion.featureLevel()));
     }
 
-    public static ApiMessageAndVersion scramMessage(String scramString) {
+    private static ApiMessageAndVersion scramMessage(String scramString) {
         return withVersion(parseScram(scramString));
     }
 
-    public static UserScramCredentialRecord parseScram(String scramString) {
+    static UserScramCredentialRecord parseScram(String scramString) {
         var nameValueRecord = scramString.split("=", 2);
+        if (nameValueRecord.length != 2 || nameValueRecord[0].isEmpty() || nameValueRecord[1].isEmpty()) {
+            throw new IllegalArgumentException("Expecting scram string in the form 'SCRAM-SHA-256=[name=alice,password=alice-secret]', found '%s'. See https://kafka.apache.org/documentation/#security_sasl_scram_credentials".formatted(scramString));
+        }
         return switch (nameValueRecord[0]) {
             case "SCRAM-SHA-256", "SCRAM-SHA-512" ->
                     StorageTool.getUserScramCredentialRecord(nameValueRecord[0], nameValueRecord[1]);
             default ->
-                    throw new IllegalArgumentException("The scram mechanism in " + scramString + " is not supported.");
+                    throw new IllegalArgumentException("The scram mechanism in '" + scramString + "' is not supported.");
         };
     }
 

kafka-server/src/test/java/com/ozangunalp/kafka/server/StorageTest.java
@@ -1,0 +1,41 @@
+package com.ozangunalp.kafka.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.apache.kafka.clients.admin.ScramMechanism;
+import org.apache.kafka.common.metadata.UserScramCredentialRecord;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StorageTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"SCRAM-SHA-256=[name=alice,password=alice-secret]",
+                            "SCRAM-SHA-256=[name=alice,iterations=8192,salt=\"MWx2NHBkbnc0ZndxN25vdGN4bTB5eTFrN3E=\",saltedpassword=\"mT0yyUUxnlJaC99HXgRTSYlbuqa4FSGtJCJfTMvjYCE=\"]"}
+    )
+    void legalScramStringAccepted(String scramString) {
+        var uscr = Storage.parseScram(scramString);
+        assertThat(uscr)
+                .extracting(UserScramCredentialRecord::mechanism)
+                .isEqualTo(ScramMechanism.SCRAM_SHA_256.type());
+        assertThat(uscr)
+                .extracting(UserScramCredentialRecord::name)
+                .isEqualTo("alice");
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"", "foo", "foo=", "="})
+    void illegalScramStringDetected(String scramString) {
+        assertThatThrownBy(() -> Storage.parseScram(scramString))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void badMechanismName() {
+        var scramString = "UNKNOWN=[name=alice,password=alice-secret]";
+        assertThatThrownBy(() -> Storage.parseScram(scramString))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

commit be5317006789852e228ba8d2e2a81074fcba5493

Fix javadoc error introduced by 3fd9fa9694b399dd7bd76b2c51fcd72777cb01e8
Signed-off-by: Keith Wall <kwall@apache.org>

kafka-server/src/main/java/com/ozangunalp/kafka/server/ServerConfig.java
@@ -37,10 +37,10 @@ public interface ServerConfig {
      * List of scram credentials, separated by semicolon.
      * <br/>
      * Format of the scram string must be in one of the following forms:
-     * <plaintext>
+     * <pre>
      * SCRAM-SHA-256=[user=alice,password=alice-secret]
      * SCRAM-SHA-512=[user=alice,iterations=8192,salt="N3E=",saltedpassword="YCE="]
-     * </plaintext>
+     * </pre>
      *
      * @return list of scram credentials
      */